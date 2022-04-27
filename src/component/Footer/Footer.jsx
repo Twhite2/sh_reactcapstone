45fba0a95e1efce6d43fcc5e21f1d8fdd8fe95c5
Twhite2 <emmanuelmieye@gmail.com>
2022-04-27 20:52:20 +0100

    about

--- src/component/Footer/Footer.jsx
+++ src/component/Footer/Footer.jsx
@@ -2,7 +2,22 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div>
+      <div class="row">
+  <div class="column">
+    <h2>Contact us</h2>
+    <hr>
+    <p>Email: customercare@admin.com</p>
+    <br>
+    <p>phone Number: +224 845 323 948</p>
+
+  </div>
+  <div class="column"></div>
+  <div class="column"></div>
+</div>
+    </div>
+
+    
   )
 }
 
